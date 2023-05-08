47996a7ee82e6df8ceee1b14c014d14acf46195c
@gichan <nototl99@gmail.com>
2023-05-08 19:42:25 +0900

    2강 완료

--- fresh/app/page.js
+++ fresh/app/page.js
@@ -1,8 +1,10 @@
 
 export default function Home() {
+  let name = 'park'
   return (
     <div>
-      
+      <h4 className="title">애플후레쉬</h4>
+      <p className="title-sub">by dev {name}</p>
     </div>
   )
 }
