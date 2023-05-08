52cb3210192fb44bab347bc1ee1a8f6ffd58ba6a
@gichan <nototl99@gmail.com>
2023-05-08 20:05:23 +0900

    강의 4 전반부 + 퀴즈

--- fresh/app/page.js
+++ fresh/app/page.js
@@ -4,10 +4,7 @@ export default function Home() {
   let name = 'park'
   return (
     <div>
-      <div className="navbar">
-        <Link href="/">홈</Link>
-        <Link href="/list">List</Link>
-      </div>
+      
       <h4 className="title">애플후레쉬</h4>
       <p className="title-sub">by dev {name}</p>
     </div>
