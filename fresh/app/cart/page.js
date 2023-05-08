aad57ecda38c2c4a14b57daff7dba32a8b171ba4
@gichan <nototl99@gmail.com>
2023-05-08 22:31:18 +0900

    6강완 & 무료 강의 끝

--- fresh/app/cart/page.js
+++ fresh/app/cart/page.js
@@ -1,17 +1,28 @@
+import { age, name } from './data.js'
+import 하이 from './hello.js'
 export default function Cart() {
   return (
     <div>
+      <하이/>
       <h4 className="title">Cart</h4>
       <div className="cart-item">
-        <p>상품명</p>
+        <p>상품명 {age}</p>
         <p>$40</p>
         <p>1개</p>
       </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem />
+      <CartItem />
+      <CartItem />
+    </div>
+  );
+}
+
+function CartItem() {
+  return (
+    <div className="cart-item">
+      <p>상품명</p>
+      <p>$40</p>
+      <p>1개</p>
     </div>
   );
 }
